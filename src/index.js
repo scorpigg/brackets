aeb0c74e6f8c302a5719a4679f793a8f76e1e1fd
Andrew <scorpigg@yandex.ru>
2021-03-03 19:51:51 +0400

    fix:add resolve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const open = bracketsConfig.reduce((acc, el) => [...acc, el[0]] ,[]);
+  const close = bracketsConfig.reduce((acc, el) => [...acc, el[1]] ,[]);
+  const stack = [];
+
+  for (const char of str) {
+      const op = open.indexOf(char);
+      const cl = close.indexOf(char);
+      if (op !== -1) {
+        if (op === cl) {
+          if (stack.length > 0 && stack[stack.length - 1] === cl) {
+            stack.pop();
+          } else {
+            stack.push(op);
+          }
+        } else {
+          stack.push(op);
+        }
+      } else { 
+        if (stack.length > 0) {
+          if (cl !== stack.pop()) {
+            return false;
+          }
+        } else {
+          return false;
+        }
+      }
+  }
+return stack.length === 0;
 }
